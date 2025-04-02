4451052578574180f2af1cd420b68cd26fd97dcc
Sabatino Masala <Masalasabatino@gmail.com>
2025-04-02 09:24:53 +0200

    Optimization

--- app/Utils/Query.php
+++ app/Utils/Query.php
@@ -8,9 +8,9 @@ use Illuminate\Support\Facades\DB;
 class Query {
     static function execute($file, $params)
     {
-        $sql = file_get_contents(resource_path('queries/' . $file . '.sql'));
         $key = 'query-' . $file . '-' . md5(serialize($params));
-        return Cache::remember($key, 60, function() use ($sql, $params) {
+        return Cache::remember($key, 60, function() use ($file, $params) {
+            $sql = file_get_contents(resource_path('queries/' . $file . '.sql'));
             return DB::select($sql, $params);
         });
     }
